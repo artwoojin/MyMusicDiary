f56f12ad8921736345cb6d5dd49f2216a1e73407
vxy2715 <vxy2715@gmail.com>
2023-04-24 17:48:37 +0900

    feat : s3 image post했을때 누적으로 저장되는게 아니라 기존이미지 삭제후 포스팅 기능

--- server/src/main/java/com/seb42/main30/seb42_main_030/image/FileUploadService.java
+++ server/src/main/java/com/seb42/main30/seb42_main_030/image/FileUploadService.java
@@ -15,43 +15,19 @@ import java.util.UUID;
 @Service
 public class FileUploadService {
 
-//    private final UploadService s3Service;
-//
-//    //Multipart를 통해 전송된 파일을 업로드하는 메소드
-//    public String uploadImage(MultipartFile file) throws IllegalAccessException {
-//        String fileName = createFileName(file.getOriginalFilename());
-//        ObjectMetadata objectMetadata = new ObjectMetadata();
-//        objectMetadata.setContentLength(file.getSize());
-//        objectMetadata.setContentType(file.getContentType());
-//        try (InputStream inputStream = file.getInputStream()) {
-//            s3Service.uploadFile(inputStream, objectMetadata, fileName);
-//        } catch(IOException e) {
-//            throw new IllegalAccessException(String.format("파일 변환 중에러가 발생하였습니다.(%s)", file.getOriginalFilename()));
-//        }
-//        return s3Service.getFileUrl(fileName);
-//    }
-//
-//    //기존 확장자명을 유지한 채, 유니크한 파일의 이름을 생성하는 로직
-//    private String createFileName(String originalFileName) throws IllegalAccessException {
-//        return UUID.randomUUID().toString().concat(getFileExtension(originalFileName));
-//    }
-//
-//    //파일의 확장자명을 가져오는 로직
-//    private String getFileExtension(String fileName) throws IllegalAccessException {
-//        try {
-//            return fileName.substring(fileName.lastIndexOf("."));
-//        } catch (StringIndexOutOfBoundsException e) {
-//            throw new IllegalAccessException(String.format("잘못된 형식의 파일($s) 입니다", fileName));
-//        }
-//    }
-
-
-
     private final UploadService s3Service;
     private final UserRepository userRepository;
 
-    //Multipart를 통해 전송된 파일을 업로드하는 메소드
+    // Multipart를 통해 전송된 파일을 업로드하는 메소드
     public void uploadImage(MultipartFile file, Long userId) throws IOException {
+        // 기존 이미지 삭제
+        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Invalid user ID"));
+        String oldFileName = getFileNameFromUrl(user.getImageUrl());
+        if (oldFileName != null) {
+            s3Service.deleteFile(oldFileName);
+        }
+
+        // 새 이미지 업로드
         String fileName = createFileName(file.getOriginalFilename());
         ObjectMetadata objectMetadata = new ObjectMetadata();
         objectMetadata.setContentLength(file.getSize());
@@ -61,7 +37,6 @@ public class FileUploadService {
         }
 
         // 유저 정보 업데이트
-        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Invalid user ID"));
         user.setImageUrl(s3Service.getFileUrl(fileName));
         userRepository.save(user);
     }
@@ -78,6 +53,18 @@ public class FileUploadService {
         } catch (StringIndexOutOfBoundsException e) {
             throw new IllegalArgumentException(String.format("Invalid file format: %s", fileName));
         }
+    }
+
+    // URL에서 파일명 추출
+    private String getFileNameFromUrl(String url) {
+        if (url == null) {
+            return null;
+        }
+        int pos = url.lastIndexOf("/");
+        if (pos == -1) {
+            return null;
+        }
+        return url.substring(pos + 1);
     }
 }
 
