3dc84dea2db68f4e41a21c289fb2a67004d26a68
kimdonggu42 <kdonggoo44@gmail.com>
2023-05-04 20:31:03 +0900

    feat : 비밀번호 수정 모달 컴포넌트 분리리

--- client/src/components/Mypage/MyInfo.tsx
+++ client/src/components/Mypage/MyInfo.tsx
@@ -6,6 +6,7 @@ import { TOKEN_API } from "../../util/API";
 import { MyContext } from "../../util/MyContext";
 import defaultProfile from "../../assets/images/defaultProfile.png";
 import Modal from "../common/Modal";
+import EditPasswordMoald from "./EditPasswordMoadl";
 
 const MyInfoContainer = styled.div`
   display: flex;
@@ -182,102 +183,6 @@ const PasswordWrapper = styled.div`
   }
 `;
 
-const EditPasswordModalBack = styled.div`
-  position: fixed;
-  z-index: 999;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  background-color: rgba(0, 0, 0, 0.4);
-  display: grid;
-  place-items: center;
-`;
-
-const EditPasswordModalView = styled.div`
-  text-align: center;
-  border-radius: 4px;
-  background-color: ${(props) => props.theme.color.background};
-  width: 85vw;
-  max-width: 400px;
-  height: 408px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.19), 0 10px 10px rgba(0, 0, 0, 0.1);
-
-  > .passwordModalTitle {
-    color: ${(props) => props.theme.color.mainText};
-    font-size: ${(props) => props.theme.font.diarySubTitleSize}px;
-    font-weight: ${(props) => props.theme.font.titleWeight};
-    text-align: left;
-    padding-left: 10px;
-    margin: 30px 15px 15px 15px;
-  }
-
-  > .passwordSubTitle {
-    color: ${(props) => props.theme.color.mainText};
-    font-size: 14px;
-    font-weight: ${(props) => props.theme.font.contentWeight};
-    text-align: left;
-    padding-left: 10px;
-    margin: 10px 15px 10px 15px;
-
-    > .red {
-      color: #ec1d36;
-    }
-  }
-
-  > button {
-    font-size: ${(props) => props.theme.font.diaryContentSize}px;
-    font-weight: ${(props) => props.theme.font.titleWeight};
-    width: 50%;
-    height: 50px;
-    border: none;
-    text-decoration: none;
-    cursor: pointer;
-
-    &:hover {
-      background-color: ${(props) => props.theme.color.buttonHover};
-    }
-  }
-
-  > .CancelButton {
-    color: ${(props) => props.theme.color.subText};
-    background-color: transparent;
-    border-top: 1px solid ${(props) => props.theme.color.borderLine};
-    border-right: 0.5px solid ${(props) => props.theme.color.borderLine};
-    border-bottom-left-radius: 4px;
-  }
-
-  > .ConfirmButton {
-    color: #5d9c59;
-    background-color: transparent;
-    border-top: 1px solid ${(props) => props.theme.color.borderLine};
-    border-left: 0.5px solid ${(props) => props.theme.color.borderLine};
-    border-bottom-right-radius: 4px;
-  }
-`;
-
-const PasswordInputWrapper = styled.div`
-  margin: 30px 0 35px 0;
-
-  > .editPasswordInput {
-    font-size: 14px;
-    width: 75vw;
-    max-width: 350px;
-    height: 50px;
-    border-radius: 4px;
-    margin-bottom: 10px;
-    padding: 10px 8px 10px 8px;
-    color: ${(props) => props.theme.color.mainText};
-    border: none;
-    border: 1px solid ${(props) => props.theme.color.loginBorderLine};
-    background-color: ${(props) => props.theme.color.background};
-
-    &:focus {
-      outline: none;
-    }
-  }
-`;
-
 const MyWithdrawalContainer = styled.div`
   font-size: 15px;
   padding: 0 5px 0 5px;
@@ -321,7 +226,7 @@ const WarningText = styled.div`
   word-break: keep-all;
 `;
 
-export interface UserDataProps {
+interface UserDataProps {
   list: UserData;
   getUserData: React.Dispatch<React.SetStateAction<object>>;
 }
@@ -329,9 +234,6 @@ export interface UserDataProps {
 function MyInfo({ list, getUserData }: UserDataProps) {
   const [nickname, setNickname] = useState<string>(list.nickname);
   const [editNickname, setEditNickname] = useState<boolean>(false);
-  const [currentPassword, setCurrentPassword] = useState<string>("");
-  const [newPassword, setNewPassword] = useState<string>("");
-  const [checkNewPassword, setCheckNewPassword] = useState<string>("");
   const [editPasswordModalOpen, setEditPasswordModalOpen] = useState<boolean>(false);
   const [withDrawalModalOpen, setWithdrawalModalOpen] = useState<boolean>(false);
 
@@ -383,43 +285,40 @@ function MyInfo({ list, getUserData }: UserDataProps) {
     setNickname(e.target.value);
   };
 
-  // 유저 패스워드 patch 요청--------------------------------------------------
-  const changePassword = async () => {
-    if (newPassword === checkNewPassword) {
-      const editNewPassword = {
-        userId: list.userId,
-        nickname: list.nickname,
-        password: newPassword,
-      };
-      const res = await TOKEN_API.patch(`/users/${list.userId}`, editNewPassword);
-      getUserData(res.data);
-    }
-  };
-  // 유저 패스워드 patch 요청--------------------------------------------------
-
   // 유저 패스워드 변경 모달 오픈 이벤트 핸들러
   const openPasswordModalHandler = () => {
     setEditPasswordModalOpen(!editPasswordModalOpen);
+    document.body.style.cssText = `
+    position: fixed;
+    top: -${window.scrollY}px;
+    overflow-y: scroll;
+    width: 100%;`;
   };
 
-  // 유저 현재 패스워드 변경 체인지 이벤트
-  const onChangeCurrentPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCurrentPassword(e.target.value);
-  };
-
-  // 유저 새 패스워드 변경 체인지 이벤트
-  const onChangeNewPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNewPassword(e.target.value);
-  };
-
-  // 유저 새 패스워드 확인 변경 체인지 이벤트
-  const onChangeCheckNewPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCheckNewPassword(e.target.value);
+  // 유저 패스워드 변경 모달 클로즈 이벤트 핸들러
+  const closePasswordModalHandler = () => {
+    setEditPasswordModalOpen(!editPasswordModalOpen);
+    const scrollY = document.body.style.top;
+    document.body.style.cssText = "";
+    window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
   };
 
   // 회원 탈퇴 모달 오픈 이벤트 핸들러
-  const openModalHandler = () => {
+  const openWithdrawalModalHandler = () => {
     setWithdrawalModalOpen(!withDrawalModalOpen);
+    document.body.style.cssText = `
+    position: fixed;
+    top: -${window.scrollY}px;
+    overflow-y: scroll;
+    width: 100%;`;
+  };
+
+  // 회원 탈퇴 모달 클로즈 이벤트 핸들러
+  const closeWithdrawalModalHandler = () => {
+    setWithdrawalModalOpen(!withDrawalModalOpen);
+    const scrollY = document.body.style.top;
+    document.body.style.cssText = "";
+    window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
   };
 
   // 회원 탈퇴 delete 요청
@@ -479,46 +378,10 @@ function MyInfo({ list, getUserData }: UserDataProps) {
               수정
             </button>
             {editPasswordModalOpen ? (
-              <EditPasswordModalBack>
-                <EditPasswordModalView>
-                  <div className='passwordModalTitle'>비밀번호 변경</div>
-                  <div className='passwordSubTitle'>
-                    <span className='red'>다른 아이디/사이트에서 사용한 적 없는 비밀번호 </span>
-                  </div>
-                  <div className='passwordSubTitle'>
-                    <span className='red'>이전에 사용한 적 없는 비밀번호</span>가 안전합니다.
-                  </div>
-                  <PasswordInputWrapper>
-                    <input
-                      className='editPasswordInput'
-                      type='text'
-                      placeholder='현재 비밀번호'
-                      value={currentPassword}
-                      onChange={onChangeCurrentPassword}
-                    ></input>
-                    <input
-                      className='editPasswordInput'
-                      type='text'
-                      placeholder='새 비밀번호'
-                      value={newPassword}
-                      onChange={onChangeNewPassword}
-                    ></input>
-                    <input
-                      className='editPasswordInput'
-                      type='text'
-                      placeholder='새 비밀번호 확인'
-                      value={checkNewPassword}
-                      onChange={onChangeCheckNewPassword}
-                    ></input>
-                  </PasswordInputWrapper>
-                  <button className='CancelButton' onClick={openPasswordModalHandler}>
-                    취소
-                  </button>
-                  <button className='ConfirmButton' onClick={changePassword}>
-                    변경
-                  </button>
-                </EditPasswordModalView>
-              </EditPasswordModalBack>
+              <EditPasswordMoald
+                list={list}
+                closePasswordModalHandler={closePasswordModalHandler}
+              />
             ) : null}
           </PasswordWrapper>
           <WarningText>로그인 시 사용되는 비밀번호입니다.</WarningText>
@@ -526,7 +389,7 @@ function MyInfo({ list, getUserData }: UserDataProps) {
         <MyWithdrawalContainer>
           <MyWithdrawalWrapper>
             <div className='withdrawalTitle'>회원 탈퇴</div>
-            <button className='withdrawalBtn' onClick={openModalHandler}>
+            <button className='withdrawalBtn' onClick={openWithdrawalModalHandler}>
               회원 탈퇴
             </button>
             {withDrawalModalOpen ? (
@@ -534,7 +397,7 @@ function MyInfo({ list, getUserData }: UserDataProps) {
                 title={"정말 탈퇴 하시겠습니까?"}
                 text={`탈퇴 시 ${nickname}님이 작성한 다이어리 및 댓글이 모두 삭제되며 복구되지 않습니다.`}
                 confirmText={"탈퇴"}
-                cancelHandler={openModalHandler}
+                cancelHandler={closeWithdrawalModalHandler}
                 confirmHandler={withDrawal}
               />
             ) : null}
